cdfcce70f173d66da5f8051a12252a3fe1896c56
Kleizjd <jose.jdgo98@gmail.com>
2022-07-13 15:38:16 -0500

    ATCUALIZACION PRIMER PJ WEB

---
+++ index.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2><em><strong>PAGINA PRINCIPAL</strong></em></h2>
+    <h3>
+        <em>Empresa XYZ</em>
+    </h3><br>
+    <ol class="no-style vertical">
+        <li><a href="Views/crear_afiliado.php"><em> CREAR UN AFILIADO</em></a></li>
+        <li><a href="Views/eliminar_empleado.php"><em> ELIMINAR UN AFILIADO</em></a></li>
+        <li><a href="Views/consultarEmpleado.php"><em> CONSULTA GENERAL DE AFILIADOS</em></a></li>
+        <li><a href="Views/codigoEmpleado.php"><em> CONSULTA EMPLEADO POR DOCUMENTO</em></a></li>
+        <li><a href="Views/codigoLiquidar.php"><em> LIQUIDAR EMPLEADO POR DOCUMENTO</em></a></li>
+        <li><a href="Views/Search_Afiliado.php"><em> ACTUALIZAR AFILIADO POR DOCUMENTO</em></li>
+    </ol>
+</body>
+</html>
